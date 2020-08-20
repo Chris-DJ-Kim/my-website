9b2a150a489ce6e32b9e330f98f1175eb37a544e
Chris Kim <chrisdjkim@gmail.com>
2020-08-20 01:45:15 -0400

    Added different background images and ability to switch between them

---
+++ images-store.js
@@ -0,0 +1,23 @@
+const images = {
+  0: {
+    imageLocation: "./images/pier.jpg",
+    imageUrl: "https://unsplash.com/photos/nwxK2Znr-is",
+  },
+  1: {
+    imageLocation: "./images/mountains.jpg",
+    imageUrl:
+      "https://www.pexels.com/photo/adventure-alpine-background-black-and-white-355747/",
+  },
+  2: {
+    imageLocation: "./images/surfers.jpg",
+    imageUrl: "https://unsplash.com/photos/eNI-0vy7IBE",
+  },
+  3: {
+    imageLocation: "./images/bridge.jpg",
+    imageUrl: "https://unsplash.com/photos/NRz7SfpnqUM",
+  },
+  4: {
+    imageLocation: "./images/beach.jpg",
+    imageUrl: "https://unsplash.com/photos/UZ3V6AV5y4o",
+  },
+};
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Makes welcome message fade in and out--can shorten later by making a function
+//Any reference to 'images' object is accessed from images-store.js
+//Makes welcome message fade in and out--can shorten later by making a function
 window.onload = () => {
   const firstLine = document.getElementById("first-line");
   const secondLine = document.getElementById("second-line");
@@ -19,10 +20,47 @@ window.onload = () => {
     secondLine.style.transition = ".4s ease 2s";
     thirdLine.style.opacity = "0";
     thirdLine.style.transition = ".4s ease 3s";
-  }, 6000);
+  }, 4500);
 
   setTimeout(() => {
     backGround.style.opacity = "1";
     backGround.style.transition = "1s ease";
-  }, 10000);
+  }, 8000);
+
+  //Randomizes initial background image
+  let backgroundImage = document.getElementById("background-image");
+  //Link will be used in popup message that asks if user wants to see image source
+  let backgroundImageLink = "";
+
+  const numberOfBackgroundImages = Object.keys(images).length;
+
+  let initialImageNumber = Math.floor(Math.random() * numberOfBackgroundImages);
+
+  backgroundImage.src = images[initialImageNumber].imageLocation;
+  backgroundImageLink = images[initialImageNumber].imageUrl;
+
+  //Enable user to switch images with arrow keys
+  //In future add buffer time between arrow keys to prevent super fast swapping
+  //ALSO ADD TRANSITION EFFECTS!!!
+  let currentImageNumber = initialImageNumber;
+  setTimeout(() => {
+    window.addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          if (currentImageNumber === 0)
+            currentImageNumber = numberOfBackgroundImages - 1;
+          else currentImageNumber--;
+          backgroundImage.src = images[currentImageNumber].imageLocation;
+          break;
+        case "ArrowRight":
+          if (currentImageNumber === numberOfBackgroundImages - 1)
+            currentImageNumber = 0;
+          else currentImageNumber++;
+          backgroundImage.src = images[currentImageNumber].imageLocation;
+          break;
+        default:
+          return;
+      }
+    });
+  }, 9500);
 };
