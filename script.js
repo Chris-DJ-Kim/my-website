e7170ac2df691381db2ce38c8aaa207da77330d5
Chris Kim <chrisdjkim@gmail.com>
2020-08-19 21:00:17 -0400

    Added animated image to home page

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// function for menu hover animations
-window.onload = () => {
-  const elements = document.getElementsByTagName("i");
-  for (i = 0, len = elements.length; i < len; i++) {
-    elements[i].style.transition = ".6s ease";
-  }
-};
-
-// function for welcome message--can prob shorten later
+// Makes welcome message fade in and out--can shorten later by making a function
 window.onload = () => {
   const firstLine = document.getElementById("first-line");
   const secondLine = document.getElementById("second-line");
   const thirdLine = document.getElementById("third-line");
 
+  const backGround = document.getElementsByClassName("background")[0];
   firstLine.style.opacity = "1";
   firstLine.style.transition = "1s ease 1s";
   secondLine.style.opacity = "1";
   secondLine.style.transition = "1s ease 2s";
   thirdLine.style.opacity = "1";
   thirdLine.style.transition = "1s ease 3s";
+
   setTimeout(() => {
     firstLine.style.opacity = "0";
-    firstLine.style.transition = ".6 ease 1s";
+    firstLine.style.transition = ".4s ease 1s";
     secondLine.style.opacity = "0";
-    secondLine.style.transition = ".6 ease 2s";
+    secondLine.style.transition = ".4s ease 2s";
     thirdLine.style.opacity = "0";
-    thirdLine.style.transition = ".6 ease 3s";
+    thirdLine.style.transition = ".4s ease 3s";
   }, 6000);
+
+  setTimeout(() => {
+    backGround.style.opacity = "1";
+    backGround.style.transition = "1s ease";
+  }, 10000);
 };
